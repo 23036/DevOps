7f062c1619c163e15fee192a232b908ccfaa7584
23036 <23036@supnum.mr>
2025-01-18 13:29:45 +0000

    tahya function

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello, World!"
-
+@app.route('/about')
+def about():
+    return "This is the page profile"
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
