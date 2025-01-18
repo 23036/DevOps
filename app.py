3a3c4007b2c3882a0b7e62ea30730f436816b0e9
OummouDiop <23066@supnum.mr>
2025-01-18 13:30:21 +0000

    fonc2

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello, World!"
-
+@app.route('/main')
+def object():
+    return "Notre objectif est de collaborer"
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
